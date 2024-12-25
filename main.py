df7f28e197146be5cab024fb84a05e252fc10680
Antheablz <agblais@icloud.com>
2024-12-25 14:20:30 -0800

    playing with tkinter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #!/pm_env/bin/python3
-
+from tkinter import *
 import tkinter as tk
 from sqlalchemy import *
 
@@ -8,9 +8,20 @@ def main():
     root.title("Password Manager")
     root.configure(background="pink")
 
-    root.minsize(400, 300)
+    root.minsize(600, 500)
     root.maxsize(600, 500)
-    root.geometry("400x300")
+    root.geometry("600x500")
+
+
+    #label = tk.Label(root, text="Centered Label")
+    button = tk.Button(root, text="quit", command=root.destroy)
+    button.place(x=300, y=250, anchor="center")
+    #label.place(x=300, y=250, anchor="center")
+
+    # frame = tk.Frame(root)
+    # frame.grid()
+    # tk.Label(frame, text="hello world").grid()
+    # tk.Button(frame, text="Quit", command=root.destroy).grid(column=1, row=0)
 
     root.mainloop()
 
